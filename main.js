7fed8ac81bf8f0bd6eb2347d31aaacadc465baf7
dudapereirapmp <127161056+dudapereirapmp@users.noreply.github.com>
2024-05-08 19:38:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -13,16 +13,16 @@ const forcasenha = document.querySelector('.forca');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho(){
-    if (tamanhoSenha > 1){
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
         // tamanhoSenha = tamanhoSenha-1;
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
-function aumentaTamanho(){
-    if (tamanhoSenha < 20){
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
         // tamanhoSenha = tamanhoSenha+1;
         tamanhoSenha++;
     }
@@ -30,28 +30,28 @@ function aumentaTamanho(){
     geraSenha();
 }
 
-for (i = 0; i < checkbox.length; i++){
+for (i = 0; i < checkbox.length; i++) {
     checkbox[i].onclick = geraSenha;
 }
 
 geraSenha();
 
-function geraSenha(){
+function geraSenha() {
     let alfabeto = '';
-    if (checkbox[0].checked){
+    if (checkbox[0].checked) {
         alfabeto = alfabeto + letrasMaiusculas;
     }
-    if (checkbox[1].checked){
+    if (checkbox[1].checked) {
         alfabeto = alfabeto +letrasMinusculas;
     }
-    if (checkbox[2].checked){
+    if (checkbox[2].checked) {
         alfabeto = alfabeto + numeros;
     }
-    if (checkbox[3].checked){
+    if (checkbox[3].checked) {
         alfabeto = alfabeto + simbolos;
     }
     let senha = '';
-    for (let i= 0; i < tamanhoSenha; i++){
+    for (let i= 0; i < tamanhoSenha; i++) {
         let numeroAleatorio = math.random() * alfabeto.length;
         numeroAleatorio = math.floor(numeroAleatorio);
         senha = senha + alfabeto[numeroAleatorio];
@@ -67,7 +67,7 @@ function classificaSenha(tamanhoAlfabeto){
     forcasenha.classList.remove('fraca','media','forte');
     if (entropia > 57){
         forcasenha.classList.add('forte');
-    } else if (entropia > 35 && entropia < 57 ){
+    } else if (entropia > 35 && entropia < 57 ) {
         forcasenha.classList.add('media');
     } else if (entropia <= 35){
         forcasenha.classList.add('fraca');
